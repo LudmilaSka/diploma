4b55b6daebaea4827fc66975460ff33833d198a5
LudmilaSka <121397319+LudmilaSka@users.noreply.github.com>
2023-02-18 22:48:03 +0300

    Add files via upload

--- fun.py
+++ fun.py
@@ -2,118 +2,126 @@ import vk_api
 from config import user_token, comm_token, offset, line
 import datetime
 from random import randrange
-from bd_vkinder import *
-
-session = vk_api.VkApi(token=user_token)
-vk = session.get_api()
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 
-def write_msg(user_id, message):
-    msg = session.method("messages.send",{"user_id" : user_id, 'message': message,'random_id': randrange(10 ** 7) })
-
-def get_name(user_id):
-    name = session.method("users.get",{"user_id" : user_id})
-    for i in name:
-        first_name = i.get('first_name')
-        return first_name
-
+class Bot:
+    def __init__(self):
+        print('Bot was created')
+        self.session = vk_api.VkApi(token=user_token)
+        self.vk = self.session.get_api()
+        self.session_group = vk_api.VkApi(token=comm_token) 
+        
 
 
-def get_sex(user_id):
-    sex = session.method("users.get",{"user_id" : user_id, "fields" : "sex"})
-    try:
-        for i in sex:
-            if i.get('sex') == 2:
-                find_sex = 1
-                return find_sex
-            elif i.get('sex') == 1:
-                find_sex = 2
-                return find_sex
-    except AttributeError:
-        write_msg(user_id, 'Бот не может определить вашу половую принадлежность, заполните профиль')
-    
+    def write_msg(self, user_id, message):
+        msg = self.session.method("messages.send",{"user_id" : user_id, 'message': message,'random_id': randrange(10 ** 7) })
 
-def get_city(user_id):
-    city = session.method("users.get",{"user_id" : user_id, "fields" : "city"})
-    try:
-        for i in city:
-            city = i.get('city')
-            id_city = str(city.get('id'))
-            return id_city
-    except AttributeError:
-        write_msg(user_id, 'Бот не может определить ваш город, заполните профиль')
+    def user_info(self, user_id):
+        resp = self.session.method("users.get",{"user_id" : user_id, "fields" : "sex , city , bdate"})
+        try:
+            for i in resp:
+                first_name = i.get('first_name')
+            for s in resp:
+                sex = s.get('sex')
+                if sex == 2:
+                    find_sex = 1
+                elif sex == 1:
+                    find_sex = 2
+            for c in resp:
+                city = c.get('city')
+            title_city = city['title']
+            for bd in resp:
+                bdate = bd.get('bdate')
+            date_list = bdate.split('.')
+            year = int(date_list[2])
+            year_now = int(datetime.date.today().year)
+            age_from = ((year_now - year) - 5)
+            age_to = ((year_now - year) + 5)
+            dict_info = {'sex' : find_sex , 'city' : title_city, 'age_from' : age_from , 'age_to' : age_to}
+            return dict_info
+        except TypeError:
+            self.write_msg(user_id, 'Профиль не заполнен ')
 
-def search_users(sex, age_at, age_to, city_id):
-    all_persons = []
-    response = session.method("users.search",  {'sort': 1,
-                           'sex': get_sex(user_id),
-                           'status': 1,
-                           'age_from': age_at,
-                           'age_to': age_to,
-                           'has_photo': 1,
-                           'count': 25,
-                           'online': 1,
-                           'city_id': get_city(user_id)
-                           })
+    def userseach(self, user_id):
+        dict_info = self.user_info(user_id)
+        response = self.session.method("users.search", {'sort': 1,
+                                                   'sex': dict_info['sex'],
+                                                   'status': 1,
+                                                   'age_from': dict_info['age_from'],
+                                                   'age_to': dict_info['age_to'],
+                                                   'has_photo': 1,
+                                                   'count': 1,
+                                                   'fields': 'is_closed',
+                                                   'hometown': dict_info['city']
+                                                   })
+        for element in response['items']:
+            if not element["is_closed"]:
+                profile_id = element.get('id')
+     
+        return profile_id
 
-    for element in response['items']:
-        person = [
-            element['first_name'],
-            element['last_name'],
-            element['id']
-        ]
-        all_persons.append(person)
-    return all_persons
-    
-def get_photo(user_owner_id):
-    try:
-        response = session.method('photos.get',
+    def get_photo(self, user_id):
+        owner_id = self.userseach(user_id)
+
+        response = self.session.method('photos.get',
                               {
-                                  'owner_id': user_owner_id,
+                                  'access_token': user_token,
+                                  'owner_id': owner_id,
                                   'album_id': 'profile',
-                                  'count': 10,
                                   'extended': 1,
                                   'photo_sizes': 1,
                               })
-    except ApiError:
-        return 'нет доступа к фото'
-    users_photos = []
-    for i in range(10):
+        dict_photos = dict()
+        for i in response['items']:
+            photo_id = str(i["id"])
+            i_likes = i["likes"]
+            if i_likes["count"]:
+                likes = i_likes["count"]
+                dict_photos[likes] = photo_id
+        list_of_ids = sorted(dict_photos.items(), reverse=True)
+        attachments = []
+        photo_ids = []
+        for i in list_of_ids:
+            photo_ids.append(i[1])
         try:
-            users_photos.append(
-                [response['items'][i]['likes']['count'],
-                 'photo' + str(response['items'][i]['owner_id']) + '_' + str(response['items'][i]['id'])])
+            attachments.append('photo{}_{}'.format(user_id, photo_ids[0]))
+            attachments.append('photo{}_{}'.format(user_id, photo_ids[1]))
+            attachments.append('photo{}_{}'.format(user_id, photo_ids[2]))
+           
+            return attachments
         except IndexError:
-            users_photos.append(['нет фото.'])
-    return users_photos
-
-def sort_likes(photos):
-    result = []
-    for element in photos:
-        if element != ['нет фото.'] and photos != 'нет доступа к фото':
-            result.append(element)
-    return sorted(result)
+            attachments.append('photo{}_{}'.format(user_id, photo_ids[0]))
+           
+            return attachments
 
 
-def get_link(sex, age_at, age_to, city):
-    all_persons = []
-    response = session.method("users.search",  {'sort': 1,
-                           'sex': sex,
-                           'status': 1,
-                           'age_from': age_at,
-                           'age_to': age_to,
-                           'has_photo': 1,
-                           'count': 25,
-                           'online': 1,
-                           'hometown': city
-                           })
-    list_1 = response['items']
-    for person_dict in list_1:
-        if person_dict.get('is_closed') == False:
-            vk_link = 'vk.com/id' + str(person_dict.get('id'))
-            return vk_link
-    
+    def send_photo(self, user_id, message, attachments):
+
+        response = self.session.method("messages.send",
+                                       {
+                                        "user_id" : user_id,
+                                        'message': message,
+                                        'random_id': randrange(10 ** 7),
+                                        'attachment': ",".join(attachments)
+                                       })
+
+
+    def show_found_person(self, user_id):
+        try:
+            self.send_photo(user_id, 'Три фото с максимальными лайками')
+            vk_link = 'vk.com/id' + str(self.userseach(user_id))
+        except TypeError:
+             return f' Похоже просмотренны все профили из БД. \n' \
+                    f' Наберите "Искать дальше" для поиска и добавления в БД.'
+
+
+bot = Bot()
+
+
+
         
+
 
 
 
