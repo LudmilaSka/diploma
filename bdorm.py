340f4ff6aa6091d02b11fceb38290c9de7084a73
LudmilaSka <121397319+LudmilaSka@users.noreply.github.com>
2023-02-26 19:05:32 +0300

    Add files via upload

--- bdorm.py
+++ bdorm.py
@@ -1,13 +1,14 @@
 import sqlalchemy
-from sqlalchemy import sesessionmaker
-from models import create_tables, User
+from sqlalchemy.orm import sessionmaker
+from models import User
 
 
 
 DSN = "postgresql://postgres:12345678@localhost:5432/bd_vkinder"
 engine = sqlalchemy.create_engine(DSN)
 
-create_tables(engine)
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
