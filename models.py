6c0f9d23819fa5a2de32d878c92bf78e63662d89
LudmilaSka <121397319+LudmilaSka@users.noreply.github.com>
2023-04-25 14:17:49 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,29 +1,40 @@
 import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker
-from bdorm import Session
+import sqlalchemy
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config import DSN, engine
 
 
 
+engine = sqlalchemy.create_engine(DSN)
+Session = sessionmaker(bind=engine)
+session = Session()
+#metadata = MetaData
 Base = declarative_base()
-
 class User(Base):
     __tablename__ = 'user'
-    id = sq.Column(sq.Integer, primary_key=True, autoincrement=True)
-    vk_id = sq.Column(sq.Integer, unique=True)
-    profile_id = sq.Column(sq.Integer, unique=True)
 
-def create_tables(engine):
-    Base.metadata.create_all(engine)
+    id = sq.Column (sq.Integer, primary_key=True)
+    profile_id = sq.Column(sq.Integer )
+    worksheet_id = sq.Column(sq.Integer)
 
+    def __str__(self):
+        return f'User  {self.profile_id}, {self.worksheet_id}'
 
-def save_user(vk_id, profile_id):
-    new_user = User(vk_id=user_id, vk_profile=profile_id )
-    session.add(new_user)
+def save_user(profile_id, worksheet_id):
+    to_bd = User(profile_id=profile_id, worksheet_id=worksheet_id)
+    session.add(to_bd)
     session.commit()
 
 def check_profile_id(profile_id):
-    current_user_id = session.query(User).filter_by(User.profile_id.IS_NULL).all()
-    return current_user_id
+    from_bd = session.query(User).filter(User.profile_id==profile_id).all()
+    return from_bd
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    #Base.metadata.drop_all(engine)
 
 
-    
+
+
+
