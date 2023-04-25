bb652781b5b011e32db1d54ab7d9dd68715122e6
LudmilaSka <121397319+LudmilaSka@users.noreply.github.com>
2023-04-25 14:14:00 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+import sqlalchemy
+
 
 user_token = '' # токен пользователя
 comm_token = '' # токен сообщества
 
-offset = 0 #сдвиг
-line = range(0, 1000) # последовательность для перебора найденных пользователей
+offset = 0
+line = range(0, 1000)
 
+DSN = "postgresql://postgres:12345678@localhost:5432/bd_vkinder1"
+engine = sqlalchemy.create_engine(DSN)
