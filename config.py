2d6c24e15136ad8da6b6de6f68b804144dfdfe88
LudmilaSka <121397319+LudmilaSka@users.noreply.github.com>
2023-05-15 17:17:45 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import sqlalchemy
 user_token = '' # токен пользователя
 comm_token = '' # токен сообщества
 
-offset = 0
+offset = 1
 line = range(0, 1000)
 
 DSN = "postgresql://postgres:12345678@localhost:5432/bd_vkinder1"
